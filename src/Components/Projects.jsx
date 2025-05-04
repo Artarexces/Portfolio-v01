c6369ba64bb1c0ea955041ef0ed870d29abc9fc1
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-05-03 22:52:00 -0300

    update componente + css

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,56 +2,108 @@ import React from 'react'
 import portfoliov1 from "./assets/portfoliov1.png"
 import foodRecipe from "./assets/foodRecipe.png"
 
+const projectData = [
+    {
+        title: "Mi portfolio",
+        image: portfoliov1,
+        description:"Este es el portfolio que se esta mostrando actualmente. Un projecto desarollado con React y puro CSS",
+        techs: [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        ],
+        demoLink:"#",
+        codeLink:"https://github.com/Artarexces/Portfolio-v01"
+    },
+    {
+        title: "Cocinando tu receta!",
+        image: foodRecipe,
+        description: "Una app simple en la cual puedes elegir alimentos y conbinarlos, tambien da un puntaje nutricional. Uno de mis primeros proyectos en la UTN.",
+        techs: [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        ],
+        demoLink:"https://cocinando-tu-receta-omega.vercel.app/",
+        codeLink:"https://github.com/Artarexces/CocinandoTuReceta?tab=readme-ov-file"
+    },
+    {
+        title: "Mi portfolio",
+        image: portfoliov1,
+        description:"Este es el portfolio que se esta mostrando actualmente. Un projecto desarollado con React y puro CSS",
+        techs: [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        ],
+        demoLink:"#",
+        codeLink:"https://github.com/Artarexces/Portfolio-v01"
+    },
+    {
+        title: "Mi portfolio",
+        image: portfoliov1,
+        description:"Este es el portfolio que se esta mostrando actualmente. Un projecto desarollado con React y puro CSS",
+        techs: [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        ],
+        demoLink:"#",
+        codeLink:"https://github.com/Artarexces/Portfolio-v01"
+    },
+    {
+        title: "Mi portfolio",
+        image: portfoliov1,
+        description:"Este es el portfolio que se esta mostrando actualmente. Un projecto desarollado con React y puro CSS",
+        techs: [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        ],
+        demoLink:"#",
+        codeLink:"https://github.com/Artarexces/Portfolio-v01"
+    },
+    {
+        title: "Mi portfolio",
+        image: portfoliov1,
+        description:"Este es el portfolio que se esta mostrando actualmente. Un projecto desarollado con React y puro CSS",
+        techs: [
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        ],
+        demoLink:"#",
+        codeLink:"https://github.com/Artarexces/Portfolio-v01"
+    }
+]
+
 const Projects = () => {
     return (
         <section id='projects' className='projects-container'>
             <h2 className='project-title'>Projects</h2>
-            <div className='project-card fade-in'>
-                <img src={portfoliov1} alt="Miniatura del projecto" className="project-thumbnail" />
-                <h3>Mi Portfolio</h3>
-                <p>Este es el portfolio que se esta mostrando actualmente. Un projecto desarollado con React y puro CSS</p>
-                <div className='project-tech'>
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
-
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
-
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-
-                </div>
-                <div className='project-links'>
-                    <a href="#" target='_blank'>Ver projecto</a>
-                    <a href="https://github.com/Artarexces/Portfolio-v01" target='_blank'>Ver codigo</a>
-                </div>
-            </div>
-
-            <div className='project-card fade-in'>
-                <img src={foodRecipe} alt="Miniatura del projecto" className="project-thumbnail" />
-                <h3>Cocinando tu receta!</h3>
-                <p>Una app simple en la cual puedes elegir alimentos y conbinarlos para crear tu propia receta y darle un nombre, tambien da un puntaje nutricional. Este fue uno de mis primeros proyectos en la UTN. </p>
-                <div className='project-tech'>
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
-
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
-
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-
-                </div>
-                <div className='project-links'>
-                    <a href="https://cocinando-tu-receta-omega.vercel.app/" target='_blank'>Ver projecto</a>
-                    <a href="https://github.com/Artarexces/CocinandoTuReceta?tab=readme-ov-file" target='_blank'>Ver codigo</a>
-                </div>
+            <div className="projects-grid">
+                {projectData.map((project,index)=> (
+                    <div className="project-card fade-in" key={index}>
+                        <img src={project.image} alt={`Miniatura de ${project.title}`} className='project-thumbnail'/>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <div className="project-tech">
+                            {project.techs.map((techs,i)=>(
+                                <img key={i} src= {techs}/>
+                            ))}
+                        </div>
+                        <div className="project-links">
+                            <a href={project.demoLink}>Ver proyecto</a>
+                            <a href={project.codeLink}>Ver codigo</a>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
     )
