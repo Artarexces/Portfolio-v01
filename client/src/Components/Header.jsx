4bf4609129e9696062cd2734a903753b2bf01ccd
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-07-31 18:15:25 -0300

    nav & header responsive fix

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -31,17 +31,17 @@ const Header = () => {
     }, [subIndex, isDeleting])
 
     return (
-        <header className='mt-40 mb-20 ml-10 md:ml-20'>
-            <div id='about'>
-                <h1 className='text-4xl font-bold mb-2 ml-10 md:ml-20'>Hola, soy Martin👋</h1>
-                <h2 className='text-2xl font-semibold mb-4 ml-10 md:ml-20'>
-                    Desarollador{" "} 
+        <header className='mt-20 md:mt-30 mb-20'>
+            <div id='about' className='ml-8 sm:ml-12 md:ml-15'>
+                <h1 className='text-3xl font-bold mb-2'>Hola, soy Martin 👋</h1>
+                <h2 className='text-3xl font-semibold mb-4'>
+                    Desarrollador{" "}
                     <span className='text-sky-400'>
                         {text}
                     </span>
                 </h2>
-                <p className='max-w-2xl text-lg leading-relaxed text-left ml-10 md:ml-20'>
-                    Soy un entusiasta del desarrollo web con experiencia en HTML, CSS, JavaScript y React como tambien en Python, node.js, typescript, express y manejo de base de datos SQL y NoSql. Siempre estoy en busca de mejorar mis habilidades y explorar nuevas tecnologías.
+                <p className='text-base sm:text-sm md:text-base leading-relaxed max-w-xl'>
+                    Soy un entusiasta del desarrollo web con experiencia en HTML, CSS, JavaScript y React como también en Python, node.js, typescript, express y manejo de base de datos SQL y NoSql. Siempre estoy en busca de mejorar mis habilidades y explorar nuevas tecnologías.
                 </p>
             </div>
         </header>
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = () => {
     return (
       <nav className="fixed flex justify-center w-full top-2 left-1/2 transform -translate-x-1/2 z-50">
-            <ul className='flex flex-wrap justify-center items-center bg-black/80 backdrop-blur-sm rounded-full px-6 py-1 gap-4 list-none text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm'>
-                <li><a href="#about" className='text-white font-bold hover:text-cyan-200 transition'>About me</a></li>
-                <li><a href="#Skills" className='text-white font-bold hover:text-cyan-200 transition'>Skills</a></li>
-                <li><a href="#projects" className='text-white font-bold hover:text-cyan-200 transition'>Projects</a></li>
-                <li><a href="#contact" className='text-white font-bold hover:text-cyan-200 transition'>Contact</a></li>
+            <ul className='flex flex-wrap justify-center items-center bg-black/80 backdrop-blur-sm rounded-full px-6 py-1 gap-4 list-none text-sm sm:text-base md:text-base lg:text-sm xl:text-sm'>
+                <li><a href="#about" className='font-bold hover:text-cyan-200 transition'>About me</a></li>
+                <li><a href="#Skills" className='font-bold hover:text-cyan-200 transition'>Skills</a></li>
+                <li><a href="#projects" className='font-bold hover:text-cyan-200 transition'>Projects</a></li>
+                <li><a href="#contact" className='font-bold hover:text-cyan-200 transition'>Contact</a></li>
             </ul>
         </nav>
     )
